46e19aed559259f4bef329adddfc5912e047afdc
your github.com username <kandeel.a1@gmail.com>
2018-08-06 20:45:57 -0400

    create assets -index.php and style sheet

---
+++ assets/index.php
@@ -0,0 +1,13 @@
+<html>
+
+  <head>
+    <title>CMS 21iLabs</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+
+  <body>
+    <div class="container">
+      <a href="index.php" id="symbol">CMS-21iLabs</a>
+    </div>
+  </body>
+</html>
